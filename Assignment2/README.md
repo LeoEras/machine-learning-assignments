90dc34bb54370191508202046690ca0389b8070c
LeoEras <derasdelgado@mun.ca>
2025-02-26 00:05:12 -0330

    Adding question 1

---
+++ Assignment2/README.md
@@ -0,0 +1,58 @@
+# Assignment 1
+
+## Team 3
+
+We are using the libraries described on the Assignment 1 file:
+* scikit-learn 1.6.1
+* matlplotlib 3.10.0
+* pandas 2.2.3
+
+No other library was used for this assignment.
+
+## Files included in the zip
+The following files are included in the response:
+
+Assignment's question answers:
+* *question1.py*
+* *question2.py*
+* *question3.py*
+* *question4.py*
+* *Report on Alzheimer’s Disease Diagnosis Using k-Nearest Neighbors.pdf*
+* This *README.md* file
+
+Additional Files:
+* *best_KNN_find.py*
+
+## Assignment's question answers
+
+**Note:** *For all of this to work properly, 'datasets' folder **should be accessible** (in the same directory) as the python files.*
+
+To run each of the answers (1 - 3), just call:
+```
+python .\question1.py
+```
+
+Question 1 will draw some plots. To see the next one, just close the current one.
+Both question 2 and question 3 will draw a graph once. The program will finish as soon as this graph is closed.
+
+For **question 4**, the following command-line structure is expected:
+```
+python .\question4.py [dataset directory] [test file]
+```
+This question asks for the dataset directory, but we also added a second command-line argument for the *Xtest* file. This file is then converted to a pandas dataframe with the lines:
+```
+Xtest_file = sys.argv[2] # The second argument
+Xtest = pd.read_csv(Xtest_file, header=None).values
+```
+So, an example call should be:
+```
+python .\question4.py .\datasets\ .\datasets\test.sDAT.csv
+```
+
+The *Report on Alzheimer’s Disease Diagnosis Using k-Nearest Neighbors.pdf* contains our evaluation report on the KNN models' performance. 
+
+## Additional Files
+* *best_KNN_find.py*
+
+It iterates over 200 k-values using different distance metrics to get the best (k-value, distance) combination. We use this knowledge for question 4.
+We discarded the distance metrics that threw errors.
---
+++ Assignment2/question1.py
@@ -0,0 +1,47 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import KFold, cross_val_score
+
+# Reading the data
+train_df = pd.read_csv("datasets/train.csv")
+test_df = pd.read_csv("datasets/test.csv")
+
+x_dataset = train_df[train_df.columns.drop("ConcreteCompressiveStrength_MPa_Megapascals_")]
+y_labels = train_df[["ConcreteCompressiveStrength_MPa_Megapascals_"]]
+
+# First split -> Blob x, x_test, blob y, y_test
+# Using 80% for blob, 20% for test
+blob_x, x_test, blob_y, y_test = train_test_split(x_dataset, y_labels, test_size=0.2, train_size=0.8)
+# Second split -> train x, x_validation, train y, y_validation
+# Using 75% for train (75% of blob), 25% for test (25% of the blob)
+# End up with something like 60%, 20%, 20% for train, validation and test respectively
+x_train, x_cv, y_train, y_cv = train_test_split(blob_x, blob_y, test_size = 0.25, train_size =0.75)
+
+train_validation_test_regressor = LinearRegression().fit(blob_x, blob_y) # Blobs are already train + validation
+y_pred = train_validation_test_regressor.predict(x_test)
+
+print("USING THE TRAIN, VALIDATION AND TEST APPROACH")
+# TRAIN - VALIDATION - TEST SPLIT IS RANDOM, SO RANDOM RESULTS HERE, AS SEEN IN CLASS
+print(f"Mean squared error: {mean_squared_error(y_test, y_pred):.2f}")
+print(f"Coefficient of determination: {r2_score(y_test, y_pred):.2f}")
+
+# Using cross-validation (k = 5)
+# This has sense as K is large
+print("\nUSING THE CROSS VALIDATION APPROACH, K = 5")
+k_folds = KFold(n_splits = 5)
+scores = cross_val_score(LinearRegression(), x_dataset, y_labels, cv = k_folds)
+print("Cross Validation Scores: ", scores)
+print("Average CV Score: ", scores.mean())
+print("Number of CV Scores used in Average: ", len(scores))
+
+# Using cross-validation (k = 10)
+# This should perform worse as K is large (compared to k = 5)
+print("\nUSING THE CROSS VALIDATION APPROACH, K = 10")
+k_folds = KFold(n_splits = 10)
+scores = cross_val_score(LinearRegression(), x_dataset, y_labels, cv = k_folds)
+print("Cross Validation Scores: ", scores)
+print("Average CV Score: ", scores.mean())
+print("Number of CV Scores used in Average: ", len(scores))
