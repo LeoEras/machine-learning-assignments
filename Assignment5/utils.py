4c90c74b2b5ba0cf39ac5b226bac95de8f69d7fc
LeoEras <derasdelgado@mun.ca>
2025-04-13 15:38:13 -0230

    Modifying some functions to update for more than 2 classes

--- Assignment5/utils.py
+++ Assignment5/utils.py
@@ -9,26 +9,17 @@ def convert_img_to_arr(dataset_imgs):
         arr_form.append(item.flatten(order='F')) # Column-major (Fortran-style) order. Elements are read in column-by-column
     return arr_form
 
-
 # Evaluate model
-def evaluate_model(model, X_test, y_test, message):
-    print(f"\n---------Evaluating {message} model-------------")
+def evaluate_model(model, X_test, y_test, model_name, mode):
+    print(f"\n---------Evaluating {model_name} model-------------")
     y_pred = model.predict(X_test)
 
     accuracy = accuracy_score(y_test, y_pred)
-    precision = precision_score(y_test, y_pred)
-    recall = recall_score(y_test, y_pred)
-    f1 = f1_score(y_test, y_pred)
-
-    # Confusion matrix
-    cm = confusion_matrix(y_test, y_pred)
-    tn, fp, _, _ = cm.ravel()
-    specificity = tn / (tn + fp)
-    balanced_accuracy = (recall + specificity) / 2
+    precision = precision_score(y_test, y_pred, average=mode)
+    recall = recall_score(y_test, y_pred, average=mode)
+    f1 = f1_score(y_test, y_pred, average=mode)
 
     print(f"Accuracy: {accuracy:.4f}")
     print(f"Precision: {precision:.4f}")
     print(f"Recall (Sensitivity): {recall:.4f}")
-    print(f"Specificity: {specificity:.4f}")
-    print(f"Balanced Accuracy: {balanced_accuracy:.4f}")
     print(f"F1 Score: {f1:.4f}")
