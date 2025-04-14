bdd17c7763e5b14d5efec608954b9cbb0d459d90
LeoEras <derasdelgado@mun.ca>
2025-04-14 11:42:27 -0230

    Final version of q2

--- Assignment5/question2.py
+++ Assignment5/question2.py
@@ -1,18 +1,20 @@
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import GridSearchCV
-from utils import img_to_arr, evaluate_model, data_split
+from utils import evaluate_model, data_split
+from sklearn.model_selection import train_test_split
 
 def best_KNN(X_train, y_train, use_subset=False):
-    k_values_to_test = [i for i in range(1, 5)] # Takes too much time, the answer is 3
+    k_values_to_test = [i for i in range(1, 30)] # Takes too much time, the answer is 3
     param_grid = {"n_neighbors": k_values_to_test}
     knn_model = KNeighborsClassifier()
 
     # Reduce dataset size for faster testing (Optional)
-    if use_subset:
-        X_train = X_train.sample(frac=0.5, random_state=42)
-        y_train = y_train.loc[X_train.index]
+    if use_subset: # To half size, otherwise it takes 4 hours to finish (13 min per pass, 18 passes)
+        X_train, _, y_train, _ = train_test_split(X_train, y_train, test_size=.5, random_state=0, stratify=y_train)
+        # Takes way less time, about an hour!
+        # Pretty cool, huh?
 
-    grid_search = GridSearchCV(knn_model, param_grid, cv=5, scoring="accuracy")
+    grid_search = GridSearchCV(knn_model, param_grid, cv=5, scoring="accuracy", verbose=3)
     grid_search.fit(X_train, y_train)
 
     best_NN = grid_search.best_params_["n_neighbors"]
@@ -26,8 +28,7 @@ def train_KNN(X_train, y_train, best_val):
 
 if __name__ == "__main__":
     X_train, X_test, y_train, y_test = data_split()
-    X_train_arr = img_to_arr(X_train)
-    X_test_arr = img_to_arr(X_test)
-    best_val = best_KNN(X_train_arr, y_train)
-    model = train_KNN(X_train_arr, y_train, best_val)
-    evaluate_model(model, X_test_arr, y_test, "KNN", "micro")
+    # best_val = best_KNN(X_train, y_train, True) # Uncomment to suffer for 3-5 minutes. Comment next line too!!!!
+    best_val = 3
+    model = train_KNN(X_train, y_train, best_val)
+    evaluate_model(model, X_test, y_test, "KNN", "micro")
