94483342488afc2edb057e8790b759b4e4f8a275
LeoEras <derasdelgado@mun.ca>
2025-04-14 11:48:07 -0230

    Final version q3

--- Assignment5/question3.py
+++ Assignment5/question3.py
@@ -1,20 +1,20 @@
 import numpy as np
 from sklearn.svm import SVC
 from sklearn.model_selection import GridSearchCV
-from utils import img_to_arr, evaluate_model, data_split
+from utils import evaluate_model, data_split
 from sklearn.model_selection import train_test_split
 
-C_SEARCH = np.logspace(-3, 3, 3) # np.logspace(-3, 3, 9)
+C_SEARCH = np.logspace(-3, 3, 9) # np.logspace(-3, 3, 9)
 
-def train_polynomial_svm(X_train, y_train, halve=False):
+def train_polynomial_svm(X_train, y_train, use_subset=False):
     print("\nTraining Polynomial SVM using Grid Search...")
 
     param_grid = {
         "C": C_SEARCH,  # Fewer C values for speed
-        "degree": [2, 3]  # Removed degree 4, 5 for speed # "degree": [2, 3, 4] 
+        "degree": [2, 3, 4]  # Removed degree 4, 5 for speed # "degree": [2, 3, 4] 
     }
 
-    if halve: # To half size, otherwise it takes 4 hours to finish (13 min per pass, 18 passes)
+    if use_subset: # To half size, otherwise it takes 4 hours to finish (13 min per pass, 18 passes)
         X_train, _, y_train, _ = train_test_split(X_train, y_train, test_size=.5, random_state=0, stratify=y_train)
         # Takes way less time, about an hour!
         # Pretty cool, huh?
@@ -41,9 +41,7 @@ def train_final_model(X_train, y_train, best_C, best_degree):
 
 if __name__ == "__main__":
     X_train, X_test, y_train, y_test = data_split()
-    X_train_arr = img_to_arr(X_train)
-    X_test_arr = img_to_arr(X_test)
-    # best_val, best_deg = train_polynomial_svm(X_train_arr, y_train, True)
-    # C = 5.623413251903491, Degree = 2 After an hour training
-    model = train_final_model(X_train_arr, y_train, 5.623413251903491, 2)
-    evaluate_model(model, X_test_arr, y_test, "SMV-Polynomial", "micro")
+    # best_val, best_deg = train_polynomial_svm(X_train, y_train, True) # Uncommnent to suffer about 1 hour, comment the next line!!
+    best_val, best_deg = 5.623413251903491, 2
+    model = train_final_model(X_train, y_train, best_val, best_deg)
+    evaluate_model(model, X_test, y_test, "SMV-Polynomial", "micro")
